60dec85da58d5979f0d0d706b8b2f6018d183990
Geram4305 <91190611+Geram4305@users.noreply.github.com>
2022-12-12 23:22:44 -0500

    feat: dump synthetic features into csv - question 2

--- utilities.py
+++ utilities.py
@@ -26,3 +26,30 @@ def calc_min_diff(data:pd.DataFrame)->pd.DataFrame:
     '''
     data[MINUTES_DIFF_COLUMN] = (data[TIMESTAMP_OPERATING_COLUMN] - data[TIMESTAMP_SCHEDULED_COLUMN]) / pd.Timedelta(minutes=1)
     return data
+
+def calc_delay15(data:pd.DataFrame)->pd.DataFrame:
+    '''
+    Generates delay_15 column.
+    Args:
+        data(pd.DataFrame): Dataframe that needs delay_15 calculated.
+    Returns:
+        data(pd.DataFrame): Datarame with delay_15 column calculated
+    '''
+    data[DELAY_15_COLUMN] = 0
+    data.loc[data[MINUTES_DIFF_COLUMN]>15,DELAY_15_COLUMN] = 1
+    return data
+
+def calc_period_day(hour:int)->str:
+    '''
+    Generates period of day from hour of day.
+    Args:
+        hour(int): Hour of day
+    Returns:
+        str: Period of day - Morning, Afternoon or Night
+    '''
+    if (hour >=5) and (hour < 12):
+        return 'Morning'
+    elif (hour >= 12) and (hour < 19 ):
+        return 'Afteroon'
+    elif (hour >= 19) or (hour < 5):
+        return'Night'
